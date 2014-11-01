779901264ef0e85df17aba7f370c5f5891453070
FJavierGil <fjavier.gilr@gmail.com>
2014-11-01 20:05:18 +0100

    Entity Repositories

---
+++ src/list_bugs_repository.php
@@ -0,0 +1,18 @@
+<?php
+// src/list_bugs_repository.php
+
+require_once "bootstrap.php";
+
+$entityManager = GetEntityManager();
+
+$bugs = $entityManager->getRepository('Bug')->getRecentBugs();
+
+foreach ($bugs as $bug) {
+    echo $bug->getDescription() . " - " . $bug->getCreated()->format('d.m.Y') . "\n";
+    echo "    Reported by: " . $bug->getReporter()->getName() . "\n";
+    echo "    Assigned to: " . $bug->getEngineer()->getName() . "\n";
+    foreach ($bug->getProducts() as $product) {
+        echo "    Platform: " . $product->getName() . "\n";
+    }
+    echo "\n";
+}
